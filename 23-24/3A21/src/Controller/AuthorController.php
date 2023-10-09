73547b2d69a82487ee9bf14bb6be01cfad5715e1
DoussRim <rim.douss@esprit.tn>
2023-10-09 13:20:37 +0100

    DB Generation

---
+++ 23-24/3A21/src/Controller/AuthorController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    private $authors = array(
+        array('id' => 1, 'picture' => '/images/ESPRIT.jpg',
+            'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ',
+            'nb_books' => 100),
+        array('id' => 2, 'picture' => '/images/ESPRIT.jpg',
+            'username' => ' William Shakespeare',
+            'email' => ' william.shakespeare@gmail.com', 'nb_books' => 200),
+        array('id' => 3, 'picture' => '/images/ESPRIT.jpg',
+            'username' => 'Taha Hussein',
+            'email' => 'taha.hussein@gmail.com', 'nb_books' => 300));
+    
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+    #[Route('/list')]
+    public function list()
+    {
+        $authors = array(
+            array('id' => 1, 'picture' => '/images/ESPRIT.jpg',
+                'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ',
+                'nb_books' => 100),
+            array('id' => 2, 'picture' => '/images/ESPRIT.jpg',
+                'username' => ' William Shakespeare',
+                'email' => ' william.shakespeare@gmail.com', 'nb_books' => 200),
+            array('id' => 3, 'picture' => '/images/ESPRIT.jpg',
+                'username' => 'Taha Hussein',
+                'email' => 'taha.hussein@gmail.com', 'nb_books' => 300));
+        
+        return $this->render('author/list.html.twig',
+            ['auth' => $authors]);
+    }
+    #[Route('/details/{id}',name:'dd')]
+    function authorDetails($id){
+        return $this->render('author/details.html.twig',
+        ['i'=>$id,'a'=>$this->authors]);
+
+    }
+}
